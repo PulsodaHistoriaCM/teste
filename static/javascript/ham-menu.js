b30b28d1d4150ca9e605c338adac3c8a1a09188a
Vitória Beatriz Batista Brandão <132864073+VitoriaBrandao23@users.noreply.github.com>
2025-04-30 14:46:24 -0300

    att2
    página about melhorada. ver mobile de politica. fiz a estrutura do hamburguer-menu, mas falta animar com java, verificar como fazer aparecer na página.

---
+++ static/javascript/ham-menu.js
@@ -0,0 +1,6 @@
+const hamMenu = document.querySelector('.ham-menu');
+
+const offMenu = document.querySelector('.off-menu');
+
+//fazer a função de animação do menu
+//usar toggleClass para adicionar .ativado, semelhante a .rolagem
--- static/javascript/headerColorido.js
+++ static/javascript/headerColorido.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     let header = document.querySelector("#header");
 
-    // Verifica se a página NÃO é a Home e adiciona a classe 'rolagem' imediatamente
+    // Verifica se a página não é a home e adiciona a classe 'rolagem'
     if (window.location.pathname !== "/" && window.location.pathname !== "/index.html") {
         header.classList.add("rolagem");
     }   
