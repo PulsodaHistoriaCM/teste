d4776d9bcde0ca88c8346f55377aed21075e4119
Vitória Beatriz Batista Brandão <132864073+VitoriaBrandao23@users.noreply.github.com>
2025-07-05 13:34:46 -0300

    resolver o nome do usuario depois

--- static/javascript/headerColorido.js
+++ static/javascript/headerColorido.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     let header = document.querySelector("#header");
+    let userNames = document.querySelectorAll("#user-name");
 
     // Verifica se a página não é a home e adiciona a classe 'rolagem'
     if (window.location.pathname !== "/" && window.location.pathname !== "/index.html") {
         header.classList.add("rolagem");
+        userNames.classList.add("rola");
     }   
 });
